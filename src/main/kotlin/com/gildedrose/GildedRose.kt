46d8c98b7f163871262ca58d7940b3e72620de55
ivan.badia <ivan.badia@zopa.com>
2020-06-15 22:14:07 +0200

    split update quality and update sellin into two methods

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -3,17 +3,26 @@ package com.gildedrose
 class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
-        items.forEach { item -> updateQuality(item) }
+        items
+                .forEach { item ->
+                    updateSellInOf(item)
+                    updateQualityOf(item)
+                }
     }
 
-    private fun updateQuality(item: Item) {
+    private fun updateSellInOf(item: Item) {
+        if (item.name != "Sulfuras, Hand of Ragnaros") {
+            item.sellIn = item.sellIn - 1
+        }
+    }
+
+    private fun updateQualityOf(item: Item) {
         when (item.name) {
             "Aged Brie" -> {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1
 
                 }
-                item.sellIn = item.sellIn - 1
                 if (item.sellIn < 0) {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1
@@ -24,19 +33,18 @@ class GildedRose(var items: Array<Item>) {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1
 
-                    if (item.sellIn < 11) {
+                    if (item.sellIn < 10) {
                         if (item.quality < 50) {
                             item.quality = item.quality + 1
                         }
                     }
 
-                    if (item.sellIn < 6) {
+                    if (item.sellIn < 5) {
                         if (item.quality < 50) {
                             item.quality = item.quality + 1
                         }
                     }
                 }
-                item.sellIn = item.sellIn - 1
                 if (item.sellIn < 0) {
                     item.quality = item.quality - item.quality
                 }
@@ -48,7 +56,6 @@ class GildedRose(var items: Array<Item>) {
                 if (item.quality > 0) {
                     item.quality = item.quality - 1
                 }
-                item.sellIn = item.sellIn - 1
                 if (item.sellIn < 0) {
                     if (item.quality > 0) {
                         item.quality = item.quality - 1
