f375c367956905c7334304ab9a08ebab0a31b61f
ivan.badia <ivan.badia@zopa.com>
2020-06-15 23:48:47 +0200

    make calculateQuality simpler

--- src/main/kotlin/com/gildedrose/quality/QualityCalculation.kt
+++ src/main/kotlin/com/gildedrose/quality/QualityCalculation.kt
@@ -2,14 +2,16 @@ package com.gildedrose.quality
 
 import com.gildedrose.Item
 import com.gildedrose.Products
+import java.lang.Integer.max
 import java.lang.Integer.min
 
 private const val MAX_QUALITY_ALLOWED = 50
+private const val MIN_QUALITY_ALLOWED = 0
 
 fun calculateAgedBrieQuality(): (Item) -> Item = { item ->
     var quality = item.quality + 1
 
-    if (item.sellIn < 0) {
+    if (sellByDateHasPassed(item)) {
         quality += 1
     }
 
@@ -21,41 +23,14 @@ fun calculateBackstageQuality(): (Item) -> Item = { item ->
     Item(item.name, item.sellIn, min(calculateBackstageQualityFor(item), MAX_QUALITY_ALLOWED))
 }
 
-private fun calculateBackstageQualityFor(item: Item): Int {
-    if (concertIsOver(item.sellIn)) {
-        return 0
-    }
-    var quality = item.quality + 1
-    if (concertIsWithinSixAndTenDays(item)) {
-        quality += 1
-    } else if (concertIsInFiveOrLessDays(item)) {
-        quality += 2
-    }
-    return quality
-}
-
-private fun concertIsInFiveOrLessDays(item: Item) = item.sellIn < 5
-
-private val WITHIN_SIX_AND_TEN_DAYS = 5..9
-
-private fun concertIsWithinSixAndTenDays(item: Item) = item.sellIn in WITHIN_SIX_AND_TEN_DAYS
-
-private fun concertIsOver(sellIn: Int) = sellIn <= 0
-
 
 fun calculateQuality(): (Item) -> Item = { item ->
-    var quality = item.quality
-    if (quality > 0) {
+    var quality = item.quality - 1
+    if (sellByDateHasPassed(item)) {
         quality -= 1
     }
-    if (item.sellIn < 0) {
-        if (quality > 0) {
-            quality -= 1
-        }
-    }
-    Item(item.name, item.sellIn, quality)
+    Item(item.name, item.sellIn, max(quality, MIN_QUALITY_ALLOWED))
 }
-
 
 fun calculateSulfurasQuality(): (Item) -> Item = { it }
 
@@ -69,4 +44,23 @@ fun getQualityCalculationFor(itemName: String): (Item) -> Item {
     return QUALITY_FUNCTIONS.getOrDefault(itemName, calculateQuality())
 }
 
+private fun sellByDateHasPassed(item: Item) = item.sellIn < 0
 
+private fun concertIsInFiveOrLessDays(item: Item) = item.sellIn < 5
+
+private fun concertIsWithinSixAndTenDays(item: Item) = item.sellIn in 5..9
+
+private fun concertIsOver(sellIn: Int) = sellIn <= 0
+
+private fun calculateBackstageQualityFor(item: Item): Int {
+    if (concertIsOver(item.sellIn)) {
+        return MIN_QUALITY_ALLOWED
+    }
+    var quality = item.quality + 1
+    if (concertIsWithinSixAndTenDays(item)) {
+        quality += 1
+    } else if (concertIsInFiveOrLessDays(item)) {
+        quality += 2
+    }
+    return quality
+}
